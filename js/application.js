882e17f943974dd3389f2e8796d54cb4de2ae80c
Daniel Deutsch & Torey Hickman <pair+common-nighthawk.toreyhickman@devbootcamp.com>
2016-07-24 17:39:10 -0500

    Renders the details and state list for a trip

--- js/application.js
+++ js/application.js
@@ -1,10 +1,40 @@
-var Welcome = React.createClass({
+// React Components
+var TripComponent = React.createClass({
+  componentDidMount: function() {
+    console.log(this.props.trip)
+  },
+
   render: function() {
-    return (<h1>Hello World!</h1>);
+    return(
+      <div>
+        <section id="trip-details">
+          <h1>{this.props.trip.name}</h1>
+          <ul>
+            <li>Origin: {this.props.trip.origin}</li>
+            <li>Destination: {this.props.trip.destination}</li>
+          </ul>
+        </section>
+        <section>
+          <ul id="states-list">
+            { this.props.trip.states.map(function(state) {
+                return(
+                  <li key={state.name.toLowerCase()}>{state.name}</li>
+                )
+              })
+            }
+          </ul>
+        </section>
+      </div>
+    );
   }
 });
 
+
+// Initializing the application
+var states = USStateListCreator();
+var trip   = new Trip("Spring Break", "Toledo", "Brooklyn", states);
+
 ReactDOM.render(
-  <Welcome />,
-  document.getElementById('example')
+  <TripComponent trip={trip} />,
+  document.getElementById('state-list')
 );
