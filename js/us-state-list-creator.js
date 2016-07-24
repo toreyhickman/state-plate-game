46d7064fb889f3afb7e7500258c9273167d6bb0f
Daniel Deutsch & Torey Hickman <pair+common-nighthawk.toreyhickman@devbootcamp.com>
2016-07-24 15:46:48 -0500

    Creates a partial list of US States in JS

---
+++ js/us-state-list-creator.js
@@ -0,0 +1,8 @@
+var USStateListCreator = function() {
+  return [
+    { abbreviation: "AL", name: "Alabama", seen: false },
+    { abbreviation: "AK", name: "Alaska", seen: false },
+    { abbreviation: "AZ", name: "Arizona", seen: false },
+    { abbreviation: "AR", name: "Arkansas", seen: false }
+  ];
+}
